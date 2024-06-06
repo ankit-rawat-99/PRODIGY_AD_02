9b8ab7dd6606a701e826a62e110fb05dc3c8d202
Ankit Rawat <ankit.rawat99work@gmail.com>
2024-06-06 09:35:27 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "prodigy_AD_02_To_Do_List"
+include(":app")
+ 
